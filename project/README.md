9e94ff2fd3cbe401953634d26eeae6d89df36ee1
Paweł Knapczyk <Pawlak.lis@gmail.com>
2023-01-24 23:23:38 +0100

    Fix game enviroment, progress in learing

--- project/README.md
+++ project/README.md
@@ -10,3 +10,17 @@ Algorytmy Stable Baselines (3-5?) - https://github.com/hill-a/stable-baselines
 Gymnasium API - https://github.com/Farama-Foundation/Gymnasium
 
 DDPG
+
+# Wykonanie projektu
+### Implementacja środowiska
+W związku z tym że Stable Baselines3 nie działa jeszcze z Gymnasium byłem zmuszony użyć gym'a
+
+Folder game2048 zawiera klasę naszego środowska które implementuje zasady gry. Nasza gra posiada 2 systemy wyświetlania, gdy chcemy pokazać graczowi plansze, oraz gdy przeprowadzamy uczneie
+
+Zbudowane środowisko musimy zarejestrować i zainstalować jako dependencję
+
+Folder logs zawiera logi uczenia które możemy przeglądać przy użyciu Tensorboard 
+
+Folder models zawiera zapisane które wytrenowaliśmy
+
+Proces uczenia opisany jest w project.ipynb
--- project/game2048/envs/env.py
+++ project/game2048/envs/env.py
@@ -9,23 +9,31 @@ from gym.vector.utils import spaces
 class game2048Env(gym.Env):
     metadata = {"render_modes": ["human", "rgb_array", "ansi"], "render_fps": 4}
 
-    def __init__(self, render_mode=None, size=4):
+    def __init__(self, reward_type=0, render_mode=None, observation_mode=1, size=4):
         super(game2048Env, self).__init__()
         self.size = size
         self.window_size = 512
 
-        self.observation_shape = (size, size)
-        self.observation_space = spaces.Box(low=np.zeros(self.observation_shape),
-                                            high=np.ones(self.observation_shape),
-                                            dtype=np.float64)
+        self.observation_mode = observation_mode
+
+        if observation_mode == 0:
+            self.observation_shape = (size, size)
+            self.observation_space = spaces.Box(low=np.zeros(self.observation_shape),
+                                                high=np.ones(self.observation_shape),
+                                                dtype=np.float64)
+        if observation_mode == 1:
+            self.observation_shape = (size, size, 16)
+            self.observation_space = spaces.Box(0, 1, self.observation_shape, dtype=int)
+
 
         self.action_space = spaces.Discrete(4)
 
         self.board = None
         # self.np_random = np.random
-        self.reward_type = 0
+        self.reward_type = reward_type
         self.sum_of_merges = 0
-        self.nr_of_moved_squeres = 0
+        self.nr_of_moved_squares = 0
+        self.nr_of_steps = 0
 
         assert render_mode is None or render_mode in self.metadata["render_modes"]
         self.render_mode = render_mode
@@ -36,21 +44,42 @@ class game2048Env(gym.Env):
     def set_reward_type(self, reward_type=0):
         self.reward_type = reward_type
 
-    def _get_obs(self):
-        return self.board / (2 ** 11)
+    def _get_obs(self, layers=16):
+        if self.observation_mode == 0:
+            return self.board / (2 ** 11)
+        if self.observation_mode == 1:
+            representation = 2 ** (np.arange(layers, dtype=int) + 1)
+
+            # layered is the flat board repeated layers times
+            layered = np.repeat(self.board[:, :, np.newaxis], layers, axis=-1)
+
+            # Now set the values in the board to 1 or zero depending whether they match representation.
+            # Representation is broadcast across a number of axes
+            layered = np.where(layered == representation, 1, 0)
+            return layered
 
     def _get_reward(self):
+        if np.max(self.board) == 2048:
+            return float( 2 ** 14)
         if self.reward_type == 0:
             return np.sum(self.board) / 4096
         if self.reward_type == 1:
             return np.max(self.board) / 4096
         if self.reward_type == 2:
-            return self.sum_of_merges
+            return self.nr_of_steps
         if self.reward_type == 3:
-            return self.sum_of_merges - self.nr_of_moved_squeres
+            return self.sum_of_merges
+        if self.reward_type == 4:
+            return self.sum_of_merges - self.nr_of_moved_squares
+        if self.reward_type == 5:
+            if self.sum_of_merges != 0:
+                return math.log2(self.sum_of_merges) - self.nr_of_moved_squares
+            return self.nr_of_moved_squares
 
-    def reset(self, seed=None):
+    def reset(self):
         # self.np_random = np.random.seed(0)
+        self.nr_of_steps = 0
+
 
         self.board = np.zeros((self.size, self.size), dtype=np.int64)
         self._place_random_tiles(count=2)
@@ -63,24 +92,33 @@ class game2048Env(gym.Env):
         return observation
 
     def step(self, action):
+        self.nr_of_moved_squares = 0
         self.sum_of_merges = 0
-        self.nr_of_moved_squeres = 0
+        self.nr_of_steps += 1
 
+        board_copy = self.slide(action)
 
-        nr_of_merges = self.slide(action)
-
+        reward = self._get_reward()
         terminated = self.is_done()
 
         if not terminated:
-            self._place_random_tiles(count=1)
+            if np.array_equal(board_copy, self.board):
+                reward = float(- 2 ** 14)
+                terminated = True
+                if np.max(self.board) == 2048:
+                    reward = float( 2 ** 14)
+            self.board = board_copy
+
+            if not terminated:
+                self._place_random_tiles(count=1)
 
         observation = self._get_obs()
-        reward = self._get_reward()
 
         if self.render_mode == "human":
             self._render_frame()
 
-        return observation, reward, terminated, {"board": self.board, "reward": self._get_reward()}
+        return observation, reward, terminated, {"board": self.board, "reward": self._get_reward(),
+                                                 "highest_tile": np.max(self.board)}
 
     def render(self, mode="rgb_array"):
         if mode == "human":
@@ -118,7 +156,7 @@ class game2048Env(gym.Env):
             for y in range(self.size):
                 if self.board[x, y] != 0:
                     color_scale = math.log2(self.board[x, y])
-                    color_scale_bounded = max(0, int(255 - 255*color_scale/10))
+                    color_scale_bounded = max(0, int(255 - 255 * color_scale / 10))
                     color = (color_scale_bounded, color_scale_bounded, 0)
                     pygame.draw.rect(
                         canvas,
@@ -145,27 +183,29 @@ class game2048Env(gym.Env):
             pygame.quit()
 
     def _place_random_tiles(self, count):
-        for _ in range(count):
-            a = np.random.randint(self.size, size=1)
-            b = np.random.randint(self.size, size=1)
-            while self.board[a, b] != 0:
+        if not self.board.all():
+            for _ in range(count):
                 a = np.random.randint(self.size, size=1)
                 b = np.random.randint(self.size, size=1)
+                while self.board[a, b] != 0:
+                    a = np.random.randint(self.size, size=1)
+                    b = np.random.randint(self.size, size=1)
 
-            # if sum([cell for row in self.board for cell in row]) in (0, 2):
-            self.board[a, b] = 2
-            # else:
-            #     self.board[a][b] = self.board.choice((2, 4))
+                # if sum([cell for row in self.board for cell in row]) in (0, 2):
+                self.board[a, b] = 2
+                # else:
+                #     self.board[a][b] = self.board.choice((2, 4))
 
     def slide(self, action):
-        self.board = np.rot90(self.board, action)
-        self.board = self.slide_down(self.board)
-        nr_of_merges, self.board = self.merge_down(self.board)
-        self.board = self.slide_down(self.board)
-        self.board = np.rot90(self.board, 4 - action)
-        return nr_of_merges
+        board_copy = self.board.copy()
+        board_copy = np.rot90(board_copy, action)
+        board_copy = self.slide_down(board_copy)
+        board_copy = self.merge_down(board_copy)
+        board_copy = self.slide_down(board_copy)
+        board_copy = np.rot90(board_copy, 4 - action)
+        return board_copy
 
-    def slide_down(self, board):
+    def slide_down(self, board, is_test=False):
         for x in range(self.size):
             for y in range(self.size - 2, -1, -1):
                 if board[y, x] == 0:
@@ -177,11 +217,12 @@ class game2048Env(gym.Env):
                 if next_y != y:
                     board[next_y, x] = board[y, x]
                     board[y, x] = 0
-                    self.nr_of_moved_squeres += 1
+                    if not is_test:
+                        self.nr_of_moved_squares += 1
 
         return board
 
-    def merge_down(self, board):
+    def merge_down(self, board, is_test=False):
         nr_of_merges = 0
         for x in range(self.size):
             for y in range(self.size - 2, -1, -1):
@@ -192,9 +233,22 @@ class game2048Env(gym.Env):
                     board[y + 1, x] = board[y, x] * 2
                     board[y, x] = 0
                     nr_of_merges += 1
-                    self.sum_of_merges += board[y, x] * 2
+                    if not is_test:
+                        self.sum_of_merges += board[y + 1, x] * 2
 
-        return nr_of_merges, board
+        return board
 
     def is_done(self):
-        return bool(self.board.all())
+        copy_board = self.board.copy()
+        if not self.board.all():
+            return False
+
+        for i in range(4):
+            copy_board = np.rot90(copy_board, k=1)
+            copy_board = self.slide_down(copy_board, is_test=True)
+            copy_board = self.merge_down(copy_board, is_test=True)
+            copy_board = self.slide_down(copy_board, is_test=True)
+            if not copy_board.all():
+                return False
+
+        return True
